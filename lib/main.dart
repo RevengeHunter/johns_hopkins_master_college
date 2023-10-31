9b66eb6d3242dc2754669d711b45370499a33889
vladimir <cdtvladimir0992@gmail.com>
2023-10-31 11:45:10 -0500

    fix: se elimina importacion de libreria innecesaria

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
